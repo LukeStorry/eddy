2964d0c9d082ce02309cf4ddec1d91490865818e
Luke Storry <luke.storry@letsdothis.com>
2023-04-01 15:11:22 +0100

    make static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<script lang="ts">
+<script>
 	import '../app.css';
 </script>
 
-<h1 class="bg-slate-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="bg-slate-500 text-center w-full h-40">Eddy's Journey</h1>
+<div class="h-full">Map goes here</div>
