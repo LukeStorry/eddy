ed5c55c719d1069155dbf9257b7abafc841ecff2
Luke Storry <luke.storry@letsdothis.com>
2023-04-02 21:40:04 +0100

    add map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,47 @@
 <script lang="ts">
+	import { PUBLIC_MAPBOX_TOKEN } from '$env/static/public';
 	import '../app.css';
-	import data from '../data.json';
+	import locations from '../data.json';
+	import { Map, Marker, controls } from '@beyonk/svelte-mapbox/components';
 
-	const dataString = JSON.stringify(data, null, 2);
+	const { GeolocateControl, NavigationControl, ScaleControl } = controls;
+
+	// const locationEdges = locations.reduce(
+	// 	([west, south, east, north], { latitude, longitude }) => [
+	// 		Math.min(west, longitude),
+	// 		Math.min(south, latitude),
+	// 		Math.max(east, longitude),
+	// 		Math.max(north, latitude)
+	// 	],
+	// 	[100, 100, -100, -100]
+	// );
 </script>
 
-<h1 class="bg-slate-500 text-center w-full h-20">Eddy's Journey</h1>
-<div class="h-full h-80">Map goes here</div>
+<h1 class="bg-slate-500 text-white text-center w-full">Eddy's Journey</h1>
+<div class="h-screen">
+	<Map
+		accessToken={PUBLIC_MAPBOX_TOKEN}
+		options={{ bounds: [-3.1, 50, 1, 53], maxBounds: [-3.1, 50, 1, 53] }}
+	>
+		<NavigationControl />
+		<GeolocateControl />
+		<ScaleControl />
+		{#each locations as l}
+			<Marker lat={l.latitude} lng={l.longitude}>
+				<svg height="20" width="20">
+					<circle cx="10" cy="10" r="8" fill="#42b883" stroke="black" />
+				</svg>
 
-<pre class="bg-slate-900 text-emerald-200 text-sm p-4">
-	{dataString}
-</pre>
+				<div slot="popup">
+					<div>
+						{new Date(l.date).toDateString()}
+					</div>
+					<div>
+						{l.description}
+					</div>
+					<img src={l.image} alt={'Photo of Eddy at ' + l.description} />
+				</div>
+			</Marker>
+		{/each}
+	</Map>
+</div>
